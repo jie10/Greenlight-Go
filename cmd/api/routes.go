e708b9c488ba218a3756e1bd816efb8f030cb9c0
Jie Burillo <benjieburillo10@gmail.com>
2024-01-21 13:33:51 +0800

    Add movie-related handlers in routes.go file
    Implemented two new handler functions in the routes.go: createMovieHandler and showMovieHandler. These two handlers will manage the POST request for creating a new movie and the GET request for retrieving a movie by its ID respectively.

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,6 +6,8 @@ func (app *application) routes() *flow.Mux {
 	router := flow.New()
 
 	router.HandleFunc("/v1/healthcheck", app.healthcheckHandler, "GET")
+	router.HandleFunc("/v1/movies", app.createMovieHandler, "POST")
+	router.HandleFunc("/v1/movies/:id", app.showMovieHandler, "GET")
 
 	return router
 }
