2e5428464ea4c0d71f6c2cd169967109daff165a
Jie Burillo <benjieburillo10@gmail.com>
2024-01-22 10:06:05 +0800

    Add envelope type for API responses
    This commit introduces `envelope`, a new data type for handling response data in API endpoints, and refactors the `writeJSON` function to utilize this new type. This change enhances consistency and readability of the code across all endpoints and allows for potential expansion of response data.

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -8,6 +8,8 @@ import (
 	"strconv"
 )
 
+type envelope map[string]any
+
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := flow.Param(r.Context(), "id")
 
@@ -19,7 +21,7 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	js, err := json.Marshal(data)
 	if err != nil {
 		return err
