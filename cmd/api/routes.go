1fc68716515e4acb4865268f1bb79a06b2a0ecc9
Jie Burillo <benjieburillo10@gmail.com>
2024-01-22 14:08:25 +0800

    Add not found and method not allowed handlers
    Added handlers for "not found" and "method not allowed" cases in the router using 'http.HandlerFunc'. This will allow the application to show a specific response for these scenarios, rather than just the default one, improving user experience and error management.

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,9 +1,15 @@
 package main
 
-import "github.com/alexedwards/flow"
+import (
+	"github.com/alexedwards/flow"
+	"net/http"
+)
 
 func (app *application) routes() *flow.Mux {
 	router := flow.New()
+
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.HandleFunc("/v1/healthcheck", app.healthcheckHandler, "GET")
 	router.HandleFunc("/v1/movies", app.createMovieHandler, "POST")
