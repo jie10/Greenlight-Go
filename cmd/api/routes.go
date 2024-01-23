d4e2c6877fda801a5d0cb04a86e828db6c0cf75b
Jie Burillo <benjieburillo10@gmail.com>
2024-01-23 09:59:11 +0800

    Wrap router handlers with panic recovery
    Wrapped previously defined handlers for routes and server errors with a panic recovery function. This change provides protection against panics that might occur during HTTP request processing, enabling graceful recovery and improved stability.

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -8,12 +8,12 @@ import (
 func (app *application) routes() *flow.Mux {
 	router := flow.New()
 
-	router.NotFound = http.HandlerFunc(app.notFoundResponse)
-	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+	router.NotFound = app.recoverPanic(http.HandlerFunc(app.notFoundResponse))
+	router.MethodNotAllowed = app.recoverPanic(http.HandlerFunc(app.methodNotAllowedResponse))
 
-	router.HandleFunc("/v1/healthcheck", app.healthcheckHandler, "GET")
-	router.HandleFunc("/v1/movies", app.createMovieHandler, "POST")
-	router.HandleFunc("/v1/movies/:id", app.showMovieHandler, "GET")
+	router.Handle("/v1/healthcheck", app.recoverPanic(http.HandlerFunc(app.healthcheckHandler)), "GET")
+	router.Handle("/v1/movies", app.recoverPanic(http.HandlerFunc(app.createMovieHandler)), "POST")
+	router.Handle("/v1/movies/:id", app.recoverPanic(http.HandlerFunc(app.showMovieHandler)), "GET")
 
 	return router
 }
