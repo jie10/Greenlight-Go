d0b95681506292c2cc1e87f177ae4ebe78fb8279
Jie Burillo <benjieburillo10@gmail.com>
2024-01-24 17:01:00 +0800

    Replace fmt package with errors and strconv in runtime.go
    Changed the package from fmt to errors and strconv in runtime.go file. This change includes simplifying the jsonValue and improving error handling for json decoding. The updated code is more precise and easier to maintain.

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,15 +1,40 @@
 package data
 
 import (
-	"fmt"
+	"errors"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
 func (r Runtime) MarshalJSON() ([]byte, error) {
-	jsonValue := fmt.Sprintf("%d mins", r)
+	jsonValue := strconv.Itoa(int(r)) + "mins"
 
 	quotedJSONValue := strconv.Quote(jsonValue)
 	return []byte(quotedJSONValue), nil
+}
+
+func (r *Runtime) unmarshalJSON(jsonValue []byte) error {
+
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+	return nil
 }
