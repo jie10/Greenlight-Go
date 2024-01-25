cc508b1f1cad0dba3b2f250c445929fa67ce7eab
Jie Burillo <benjieburillo10@gmail.com>
2024-01-25 13:00:26 +0800

    Refactor Runtime Marshal and Unmarshal methods in runtime.go
    The methods MarshalJSON and UnmarshalJSON for the Runtime type have been updated. The changes improve error handling by adjusting the parsing of the unquotedJSONValue and by removing unnecessary checks. Furthermore, pointers are now being used for the Runtime receiver in both methods.

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -10,30 +10,22 @@ var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
-func (r Runtime) MarshalJSON() ([]byte, error) {
-	jsonValue := strconv.Itoa(int(r)) + "mins"
+func (r *Runtime) MarshalJSON() ([]byte, error) {
+	jsonValue := strconv.Itoa(int(*r)) + " mins"
 
 	quotedJSONValue := strconv.Quote(jsonValue)
 	return []byte(quotedJSONValue), nil
 }
 
-func (r *Runtime) unmarshalJSON(jsonValue []byte) error {
-	str := string(jsonValue)
-
-	if !strings.HasSuffix(str, "\"mins\"") {
-		return ErrInvalidRuntimeFormat
-	}
-	unquotedJSONValue, err := strconv.Unquote(str)
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
 	if err != nil {
 		return ErrInvalidRuntimeFormat
 	}
 
-	parts := strings.Split(unquotedJSONValue, " ")
-	if len(parts) != 2 {
-		return ErrInvalidRuntimeFormat
-	}
+	unquotedJSONValue = strings.TrimSuffix(unquotedJSONValue, " mins")
 
-	i, err := strconv.ParseInt(parts[0], 10, 32)
+	i, err := strconv.ParseInt(unquotedJSONValue, 10, 32)
 	if err != nil {
 		return ErrInvalidRuntimeFormat
 	}
