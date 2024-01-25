069930e0cc555e425471015d6a27c83a53ca28f8
Jie Burillo <benjieburillo10@gmail.com>
2024-01-25 13:54:22 +0800

    Add movie validation in the api
    Integrated a new validation functionality for movies in the api. The added code validates each movie by their details (Title, Year, Runtime, Genres) and sends a validation error response with the `failedValidationResponse` function when validation fails.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/jie10/greenlight-go/internal/data"
+	"github.com/jie10/greenlight-go/internal/validator"
 	"net/http"
 	"time"
 )
@@ -18,6 +19,19 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
