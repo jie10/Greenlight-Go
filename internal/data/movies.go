ac89789aa70cb853031a935c54e8efe1075e5889
Jie Burillo <benjieburillo10@gmail.com>
2024-01-26 10:36:17 +0800

    Add MovieModel with Insert, Get, Update, Delete methods
    In this update, we have introduced the MovieModel struct in the movies.go file. This includes the Insert, Get, Update, and Delete methods for managing movie records in the database. Additionally, a MockMovieModel struct without any methods is also defined for further development.

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,7 +1,9 @@
 package data
 
 import (
+	"database/sql"
 	"github.com/jie10/greenlight-go/internal/validator"
+	"github.com/lib/pq"
 	"time"
 )
 
@@ -13,6 +15,13 @@ type Movie struct {
 	Runtime   Runtime   `json:"runtime,omitempty"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
+}
+
+type MovieModel struct {
+	DB *sql.DB
+}
+
+type MockMovieModel struct {
 }
 
 func ValidateMovie(v *validator.Validator, movie *Movie) {
@@ -27,4 +36,24 @@ func ValidateMovie(v *validator.Validator, movie *Movie) {
 	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
 	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
 	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
+}
+
+func (m MovieModel) Insert(movie *Movie) error {
+	query := `INSERT INTO movies (title, year, runtime, genres) VALUES ($1, $2, $3, $4) RETURNING id, created_at, version`
+
+	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+}
+
+func (m MovieModel) Get(id int64) (*Movie, error) {
+	return nil, nil
+}
+
+func (m MovieModel) Update(movie *Movie) error {
+	return nil
+}
+
+func (m MovieModel) Delete(id int64) error {
+	return nil
 }
