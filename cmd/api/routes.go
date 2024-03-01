c8aabab32a8742077b1a2f26ee9a38414c45e44e
Jie Burillo <benjieburillo10@gmail.com>
2024-03-01 23:38:51 +0800

    Update API route handlers implementation
    The handlers in the routes function have been updated to standard http.HandlerFunc. The recoverPanic function, which was previously applied to individual route handlers, is now centrally applied to the entire router. This change simplifies the code and avoids redundancy. Also, rate limiting function has been introduced to prevent abuse and ensure fair usage.

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -5,20 +5,20 @@ import (
 	"net/http"
 )
 
-func (app *application) routes() *flow.Mux {
+func (app *application) routes() http.Handler {
 	router := flow.New()
 
-	router.NotFound = app.recoverPanic(http.HandlerFunc(app.notFoundResponse))
-	router.MethodNotAllowed = app.recoverPanic(http.HandlerFunc(app.methodNotAllowedResponse))
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
-	router.Handle("/v1/healthcheck", app.recoverPanic(http.HandlerFunc(app.healthcheckHandler)), "GET")
+	router.Handle("/v1/healthcheck", http.HandlerFunc(app.healthcheckHandler), "GET")
 
-	router.Handle("/v1/movies", app.recoverPanic(http.HandlerFunc(app.listMoviesHandler)), "GET")
-	router.Handle("/v1/movies", app.recoverPanic(http.HandlerFunc(app.createMovieHandler)), "POST")
+	router.Handle("/v1/movies", http.HandlerFunc(app.listMoviesHandler), "GET")
+	router.Handle("/v1/movies", http.HandlerFunc(app.createMovieHandler), "POST")
 
-	router.Handle("/v1/movies/:id", app.recoverPanic(http.HandlerFunc(app.showMovieHandler)), "GET")
-	router.Handle("/v1/movies/:id", app.recoverPanic(http.HandlerFunc(app.updateMovieHandler)), "PATCH")
-	router.Handle("/v1/movies/:id", app.recoverPanic(http.HandlerFunc(app.deleteMovieHandler)), "DELETE")
+	router.Handle("/v1/movies/:id", http.HandlerFunc(app.showMovieHandler), "GET")
+	router.Handle("/v1/movies/:id", http.HandlerFunc(app.updateMovieHandler), "PATCH")
+	router.Handle("/v1/movies/:id", http.HandlerFunc(app.deleteMovieHandler), "DELETE")
 
-	return router
+	return app.recoverPanic(app.ratelimit(router))
 }
